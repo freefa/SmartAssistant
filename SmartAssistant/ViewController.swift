4e594842e3698a3eff8037de89f550dffa03374f
lubiao <lubiao86@126.com>
2019-11-28 18:07:26 +0800

    Initial Commit

---
+++ SmartAssistant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartAssistant
+//
+//  Created by Louis.B on 2019/11/28.
+//  Copyright © 2019 Louis.B. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
